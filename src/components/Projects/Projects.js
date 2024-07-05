0ada166fad32277815e6ef51a7b0e2867ccd253e
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-07-06 03:49:00 +0530

    Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import Meet from "../../Assets/Projects/Nexmeet.png";
 import Crypto from "../../Assets/Projects/cryptoxai.png";
-
+import Mon from '../../Assets/Projects/mon.png'
 
 function Projects() {
   return (
@@ -36,6 +36,16 @@ function Projects() {
               description="CRYPTOxAI is a cutting-edge platform combining cryptocurrency exchange with AI-driven insights. It delivers real-time information and  empowers investors to make informed decisions. Our unified workspace allows users to track currency progress, predict trends, and access reliable information about companies and currencies, all in one place."
               ghLink="https://github.com/Sanghrakshit12/NitJHackathon"
               demoLink="https://cryptoxai.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Mon}
+              isBlog={false}
+              title="MoneyTransferX"
+              description="This full-stack application enables users to securely transfer money between accounts. Built with modern technologies, it leverages Express.js as the HTTP server for the backend, seamlessly connecting to MongoDB using Mongoose as the ODM for data management On the frontend, React is employed as the framework of choice, providing a dynamic and intuitive user interface for seamless interaction with the."
+              ghLink="https://github.com/Sanghrakshit12/Money-transfering-Application-Fullstack"
+              demoLink="https://money-transfering-application-fullstack.vercel.app/"
             />
           </Col>
 
