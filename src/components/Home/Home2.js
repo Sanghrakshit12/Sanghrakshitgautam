7898c912e10691fddc49865cd8cc4e13bf084c16
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-07-29 13:43:45 +0530

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/ai.jpg";
+import myImg from "../../Assets/me.jpeg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
